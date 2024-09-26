c1b7c1c3482ef5815c325f58933304439a660179
TheNovaxian <shaquille-36@hotmail.com>
2024-09-26 16:41:20 -0400

    logout button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState} from 'react';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import About from './Components/About';
@@ -8,10 +8,22 @@ import Contact from './Components/Contact';
 
 
 function App() {
+
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      <Hero isLoggedIn={isLoggedIn} onLogin={handleLogin} />
       <About/>
       <Assessment/>
       <Contact/>
