1db74d46784a22f24ff868556d093fec98775c33
TheNovaxian <shaquille-36@hotmail.com>
2024-09-28 22:37:09 -0400

    AESTHETICS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState} from 'react';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
-import About from './Components/About';
-import Assessment from './Components/Assessment';         
-import Contact from './Components/Contact';
+import About from './Components/About/About';
+import Assessment from './Components/Assessment/Assessment';         
+import Contact from './Components/Contact/Contact';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Dashboard from './Components/Dashboard/DashUser';
 
 
 
@@ -21,13 +23,37 @@ function App() {
   };
 
   return (
+    <Router>
     <div>
-      <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
-      <Hero isLoggedIn={isLoggedIn} onLogin={handleLogin} />
-      <About/>
-      <Assessment/>
-      <Contact/>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+            <Hero isLoggedIn={isLoggedIn} onLogin={handleLogin} />
+          </>
+        } />
+        <Route path="/about" element={
+          <>
+            <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+            <About />
+          </>
+        } />
+        <Route path="/assessment" element={
+          <>
+            <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+            <Assessment />
+          </>
+        } />
+        <Route path="/contact" element={
+          <>
+            <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+            <Contact />
+          </>
+        } />
+        <Route path="/dashboard" element={<Dashboard />} /> {/* No Navbar here */}
+      </Routes>
     </div>
+  </Router>
   );
 }
 
